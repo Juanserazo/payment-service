9a62e81b75533634a934fb8fea11de7c460d0666
jserazo <juanserazo@hotmail.es>
2026-07-02 19:41:56 -0500

    feat update settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,15 +1,15 @@
+from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env")
+
     app_name: str = "Payment Service"
 
     database_url: str = (
         "sqlite+aiosqlite:///./payments.db"
     )
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
